33dbb98047371f1d78eb6faed89d2e4bd34cf5a3
douglas99hm <47165630+douglas99hm@users.noreply.github.com>
2019-03-05 10:49:52 -0600

    Commit Object

---
+++ HospitalitoLaBendiciondeDios/src/java/Objects/Paciente.java
@@ -0,0 +1,16 @@
+package Objects;
+
+public class Paciente 
+{
+//variables de instancia
+private String m_strNombre;
+private String m_strApellido;
+private String m_strGenero;
+private String m_strSintomas;
+
+private int m_iBirthYear;
+private int m_iBirthMonth;
+private int m_iBirthDay;
+
+
+}
